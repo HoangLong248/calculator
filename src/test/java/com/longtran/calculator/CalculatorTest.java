b594e8215bdbe953f45069df2f18a50251022131
Hoang Long <tranhuuhoanglong248@gmail.com>
2022-07-27 00:50:23 +0700

    Add sum logic, controller and unit test

---
+++ src/test/java/com/longtran/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.longtran.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	private Calculator calculator = new Calculator();
+
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2, 3));
+	}
+}
